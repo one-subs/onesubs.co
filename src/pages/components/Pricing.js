45dfbaa668ab45ae5e30f327697b40c76f4bdebb
Omurzak <omurzak.keldibek@gmail.com>
2025-01-02 23:07:25 +0600

    update loadings and button

--- src/pages/components/Pricing.js
+++ src/pages/components/Pricing.js
@@ -11,41 +11,51 @@ function Pricing({ bottom }) {
                     <h1>S</h1>
                     <p>(one S tier/24 hours)</p>
                     <span className="price">$15</span><span className="month">/mo</span>
-                    <button class="button">
-                        <span class="button-content">Get started!</span>
-                    </button>
+                    <Link to="/account">
+                        <button class="button">
+                            <span class="button-content">Get started!</span>
+                        </button>
+                    </Link>
                 </div>
                 <div className="card">
                     <h1>M</h1>
                     <p>(one M, two S)</p>
                     <span className="price">$21</span><span className="month">/mo</span>
-                    <button class="button">
-                        <span class="button-content">Get started!</span>
-                    </button>
+                    <Link to="/account">
+                        <button class="button">
+                            <span class="button-content">Get started!</span>
+                        </button>
+                    </Link>
                 </div>
                 <div className="card">
                     <h1>L</h1>
                     <p>(1L, 2M, 4S)</p>
                     <span className="price">$36</span><span className="month">/mo</span>
-                    <button class="button">
-                        <span class="button-content">Get started!</span>
-                    </button>
+                    <Link to="/account">
+                        <button class="button">
+                            <span class="button-content">Get started!</span>
+                        </button>
+                    </Link>
                 </div>
                 <div className="card">
                     <h1>XL</h1>
                     <p>(XL, 2L, 4M, 8S)</p>
                     <span className="price">$69</span><span className="month">/mo</span>
-                    <button class="button">
-                        <span class="button-content">Get started!</span>
-                    </button>
+                    <Link to="/account">
+                        <button class="button">
+                            <span class="button-content">Get started!</span>
+                        </button>
+                    </Link>
                 </div>
                 <div className="card">
                     <h1>XXL</h1>
                     <p>(XXL, XL, 3L, 6M, 13S)</p>
                     <span className="price">$115</span><span className="month">/mo</span>
-                    <button class="button">
-                        <span class="button-content">Get started!</span>
-                    </button>
+                    <Link to="/account">
+                        <button class="button">
+                            <span class="button-content">Get started!</span>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </div>
